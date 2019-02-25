581464b5d02c0f0c8e8925992441cb36dadb6926
Rabiun <fizaa99@gmail.com>
2019-02-25 21:25:48 +0530

    Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,13 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms'; 
+
 
 import { AppComponent } from './app.component';
 
@@ -50,7 +55,11 @@ import { LoginComponent } from './login/login.component';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
     FlexLayoutModule,
+    FormsModule,
     MatDialogModule
   ],
   providers: [
