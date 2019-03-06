7e569b86adb18a1976ad5ca54ca1a41022ad27fd
Rabiun <fizaa99@gmail.com>
2019-03-06 19:57:15 +0530

    HTTP Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,13 +31,15 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 import { AppRoutingModule} from './app-routing/app-routing.module';
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
-import { LoginComponent } from './login/login.component';
+import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
+
 import { baseURL } from './shared/baseurl';
 
 
@@ -79,6 +81,7 @@ import { baseURL } from './shared/baseurl';
     DishService,
     PromotionService,
     LeaderService,
+    ProcessHTTPMsgService,
     {provide: 'baseURL', useValue: baseURL}
   ],
   entryComponents: [
