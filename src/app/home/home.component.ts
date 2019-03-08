6240850854ae9eec9adb4dcaf0b64eaa4eb3700b
Rabiun <fizaa99@gmail.com>
2019-03-08 17:01:57 +0530

    Animations Part 2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,14 +3,23 @@ import { Dish } from '../shared/dish';
 import {DishService } from '../services/dish.service';
 import { Promotion } from '../shared/promotion';
 import { PromotionService } from '../services/promotion.service';
-import { LEADERS } from '../shared/LEADERS';
 import { LeaderService } from '../services/leader.service';
 import { Leader } from '../shared/leader';
+import { flyInOut, expand } from '../animations/app.animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  // tslint:disable-next-line:use-host-property-decorator
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+    },
+    animations: [
+      flyInOut(),
+      expand()
+    ]
 })
 export class HomeComponent implements OnInit {
 
